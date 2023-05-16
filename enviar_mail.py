f1343327481f956e7a57409eb4a63374666921aa
Agustin <50929799+agusdomin@users.noreply.github.com>
2023-05-16 19:26:31 -0300

    Update enviar_mail.py

--- enviar_mail.py
+++ enviar_mail.py
@@ -12,6 +12,7 @@ def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
     cuerpo="Hola desde colab"
     msg['From']=os.getenv('USER_GMAIL')
     msg['To']=os.getenv('USER_GMAIL')
+    msg['Subject']='Hola'
     msg.set_content(cuerpo)
     server.login(user_gmail,password_gmail)
     server.send_message(msg)
