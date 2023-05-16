c4431a5fd049b4386138d0866b7b8517ef8d86eb
Agustin <50929799+agusdomin@users.noreply.github.com>
2023-05-16 18:38:31 -0300

    Update enviar_mail.py

--- enviar_mail.py
+++ enviar_mail.py
@@ -1,2 +1,15 @@
- if __name__ == "__main__":
-      enviar_mail(usuario, contraseña) # modificar segun se llame tu funcion y parametros
+import os
+import smtplib
+from email.message import EmailMessage
+
+user_gmail = os.getenv('USER_GMAIL')
+password_gmail = os.getenv('PASSWORD_GMAIL')
+
+def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
+    # aqui va tu codigo
+    ...
+
+send_notifying_mail(user_gmail, password_gmail)
+
+if __name__ == "__main__":
+      send_notifying_mail(user_gmail, password_gmail)
